8cc290081b92938d1a426d1ff4a24f89dbcce5ae
Shane Barton <shanecb@hotmail.ca>
2025-04-10 11:32:37 -0400

    Divide project into engine framework/game.
    Created separate modules for the engine and game layers to improve code organization and reusability. Modified CMakeLists for engine/game linking, removed entry point from game project and encapsulated it within the framework.

--- LightYears/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-
-#include "framework/application.h"
-
-int main()
-{
-	std::unique_ptr<ly::Application> application(new ly::Application());
-	application->Run();
-}
---
+++ LightYearsGame/src/GameFramework/GameApplication.cpp
@@ -0,0 +1,11 @@
+#include "GameFramework/GameApplication.h"
+
+ly::Application* GetApplication()
+{
+	return new ly::GameApplication{};
+}
+
+namespace ly
+{
+
+}
