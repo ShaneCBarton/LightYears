c638b54a78870abe3452b05d1119437749f595b2
Shane Barton <shanecb@hotmail.ca>
2025-04-10 11:56:44 -0400

    Add definitions for c++ data types/structures.

--- LightYearsEngine/include/framework/Core.h
+++ LightYearsEngine/include/framework/Core.h
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <map>
+#include <memory>
+#include <unordered_map>
 
 namespace ly
 {
+	template<typename T>
+	using unique = std::unique_ptr<T>;
+
+	template<typename T>
+	using shared = std::shared_ptr<T>;
+
+	template<typename T>
+	using weak = std::weak_ptr<T>;
+
+	template<typename T>
+	using List = std::vector<T>;
+
+	template<typename KeyType, typename ValueType, typename Pr = std::less<KeyType>>
+	using Map = std::map<KeyType, ValueType, Pr>;
+
+	template<typename KeyType, typename ValueType, typename Hasher = std::hash<KeyType>>
+	using Dictionary = std::unordered_map<KeyType, ValueType, Hasher>;
+
+
 #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
 }
